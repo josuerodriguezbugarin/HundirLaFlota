eb3731eb4203d8850510384344e23fc6f961ace5
josue <josuerodriguezbuarin@danielcastelao.org>
2026-03-24 13:56:28 +0100

    Examen completo y comentado

--- juego.py
+++ juego.py
@@ -1,10 +1,14 @@
 from tablero import Tablero
 
 class Juego:
+
+    #Conecta las acciones del jugador con el tablero y muestra los resultados.
     def __init__(self):
         self.tablero = Tablero()
 
     def mostrar_resultado(self, resultado):
+
+        #Traduce el código numérico devuelto por la lógica del juego a texto legible.
         if resultado == 0:
             print("Agua")
         elif resultado == 1:
@@ -15,6 +19,8 @@ class Juego:
             print("Ya disparaste aquí")
 
     def lanzar_ataque(self, x, y):
+
+        #Inicia el proceso de ataque en unas coordenadas específicas.
         print(f"\nAtaque en ({x},{y})")
 
         resultado = self.tablero.comprobar_impacto(x, y)
@@ -22,6 +28,8 @@ class Juego:
         self.mostrar_resultado(resultado)
 
     def jugar_demo(self):
+
+        #Esto es como una demo que ejecuta una serie de disparos de prueba.
         self.lanzar_ataque(1, 1)
         self.lanzar_ataque(1, 2)
         self.lanzar_ataque(1, 3)
